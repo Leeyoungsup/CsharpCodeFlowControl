8696c61cec0efe58e64baff1aa303b748cf02c3a
leeyoungsub <liive29252@gmail.com>
2020-06-06 20:25:48 +0900

    200606

--- ConstantsAndEnumerations/Program.cs
+++ ConstantsAndEnumerations/Program.cs
@@ -4,21 +4,30 @@ namespace ConstantsAndEnumerations
 {
     class Program
     {
+        enum Choice
+        {
+            Rock=1,
+            Paper=2,
+            Scissors=0
+        }
         static void Main(string[] args)
         {
+            //const int Rock=1;
+            //const int Paper=2;
+            //const int Scissors=0;
             //0:가위 1:바위 2:보
             Random rand = new Random();
             int aiChoice = rand.Next(0, 3); //0~2사이의 랜덤 값
-            int Choice = Convert.ToInt32(Console.ReadLine());
-            switch (Choice)
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
             {
-                case 0:
+                case (int)Choice.Scissors:
                     Console.WriteLine("당신의 선택은 가위입니다");
                     break;
-                case 1:
+                case (int)Choice.Rock:
                     Console.WriteLine("당신의 선택은 바위입니다");
                     break;
-                case 2:
+                case (int)Choice.Paper:
                     Console.WriteLine("당신의 선택은 보입니다");
                     break;
                 default:
@@ -27,26 +36,26 @@ namespace ConstantsAndEnumerations
             }
             switch (aiChoice)
             {
-                case 0:
+                case (int)Choice.Scissors:
                     Console.WriteLine("컴퓨터의 선택은 가위입니다");
                     break;
-                case 1:
+                case (int)Choice.Rock:
                     Console.WriteLine("컴퓨터의 선택은 바위입니다");
                     break;
-                case 2:
+                case (int)Choice.Paper:
                     Console.WriteLine("컴퓨터의 선택은 보입니다");
                     break;
             }
             //승리 , 무승부 ,패배
-            if (Choice == aiChoice)
+            if (choice == aiChoice)
             {
                 Console.WriteLine("무승부입니다.");
             }
-            else if (Choice == (aiChoice + 1) % 3)
+            else if (choice == (aiChoice + 1) % 3)
             {
                 Console.WriteLine("이겼습니다");
             }
-            else if (Choice == (aiChoice + 2) % 3)
+            else if (choice == (aiChoice + 2) % 3)
             {
                 Console.WriteLine("졌습니다");
             }
