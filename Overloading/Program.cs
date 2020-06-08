f294171675b6d32d224bfee8ff6b231ef685cc40
leeyoungsub <liive29252@gmail.com>
2020-06-08 23:29:32 +0900

    200608

---
+++ Overloading/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Overloading
+{
+    class Program
+    {
+        //함수 이름의 재사용
+        static int Add(int a, int b,int c=0,float d= 0.0f,double e=3.0)
+        {
+            Console.WriteLine("Add int 3호출");
+            return a + b+c;
+
+        }
+        static float Add(float a, float b)
+        {
+            Console.WriteLine("Add float 호출");
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            int ret= Program.Add(2, 3,2);
+            float ret1 = Program.Add(2.0f, 3.0f);
+            Console.WriteLine(ret1);
+        }
+    }
+}
