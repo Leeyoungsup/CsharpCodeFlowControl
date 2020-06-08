fd8963acc1790920f22cddd41080cc0d25dbefbb
leeyoungsub <liive29252@gmail.com>
2020-06-08 23:17:49 +0900

    200608

--- RefOut/Program.cs
+++ RefOut/Program.cs
@@ -4,9 +4,35 @@ namespace RefOut
 {
     class Program
     {
+        static void Divide(int a , int b ,out int result1 ,out int result2)
+        {
+            result1 = a / b;
+            result2 = a % b;
+        }
+        static void Swap(ref int a,ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        static void AddOne(ref int number)
+        {
+            number = number + 1;
+        }
+        static int AddOne2(int number)
+        {
+            return number + 1;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1 = 10;
+            int num2 = 3;
+            int result1;
+            int result2;
+            Divide(10, 3, out result1, out result2);
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            
         }
     }
 }
