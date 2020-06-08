fa7ebd06898d2d6fe241970116b923e73191b682
leeyoungsub <liive29252@gmail.com>
2020-06-08 17:19:25 +0900

    200608

--- function/Program.cs
+++ function/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace function
 {
     class Program
     {
+        //Method 함수
+        static void Helloworld()
+        {
+            Console.WriteLine("HelloWorld");
+        }
+        static int Add(int a, int b)
+        {
+            int result = a + b;
+;           return result;
+            
+        }
+        static void AddOne(ref int number)
+        {
+            number = number + 1;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 4;
+            int b = 5;
+            Program.AddOne(ref a);
+
+            Console.WriteLine(a);
+            //4,5=>9
         }
     }
 }
