ca65b5ef1229dd266cf11cc07eeaafdec2b677f2
leeyoungsub <liive29252@gmail.com>
2020-06-08 16:03:01 +0900

    200608

---
+++ BreakContinue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BreakContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ For/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            while(count<5)
+            {
+                Console.WriteLine("hello World!");
+                count++;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
